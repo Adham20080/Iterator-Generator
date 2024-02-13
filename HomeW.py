386b31c91de7d81cf66fe8670b526d2712c43552
ahmadjon2008 <robotoprotecno2008@gmail.com>
2024-02-13 10:44:23 +0500

    Iterator va Generator

--- HomeW.py
+++ HomeW.py
@@ -1,28 +1,25 @@
-# i = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-#
-# it = iter(i)
+it = iter([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
 
 
-# class RetGen:
-#     def __init__(self, iterator):
-#         self.ite = iterator
-#
-#     def __next__(self):
-#         b = []
-#         for a in self.ite:
-#             if a % 2 == 0:
-#                 b.append(a)
-#         return f"Toq sonlar: {b}"
-#
-#
-# obj = RetGen(it)
-# print(next(obj))
+class RetGen:
+    def __init__(self, iterator):
+        self.ite = iterator
 
-# i = [1, 3, 5, 7, 9, 11, 13, 15]
-#
-#
-# def generator(iterator):
-#     yield iterator
-#
-#
-# print(*next(generator(i)))
+    def __next__(self):
+        d = []
+        for a in self.ite:
+            d.append(a ** 2)
+        return f"Dooble: {d}"
+
+
+obj = RetGen(it)
+print(next(obj))
+
+i = [1, 3, 5, 7, 9, 11, 13, 15]
+
+
+def generator(iterator):
+    yield iterator
+
+
+print(*next(generator(i)))
